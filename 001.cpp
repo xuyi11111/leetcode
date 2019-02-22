807d32fa9eff1af959b8cf032f79446ee4aa3315
MasterXiYu <1532940772@qq.com>
2019-02-22 15:39:42 +0800

    pull new code
    add some suggestion

--- 001.cpp
+++ 001.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int len=nums.size();
+        int min,max;
         for(int i=0;i<len-1;i++){
             for(int j=i+1;j<=len-1;j++){
-                if(nums[i]+nums[j]==target){
-                     vector<int> temp;
-                     temp.push_back(i);
-                     temp.push_back(j);
-                     return temp;
+                if((nums[i]+nums[j])==target){
+                    min=i;
+                    max=j;
                 }
             }
         }
+        vector<int> temp;
+        temp.push_back(min);
+        temp.push_back(max);
+        return temp;
     }
 };
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# leetcode
+create for leetcode OJ
+this is c++ code for leetcode,I will speed one yeah to complete it
